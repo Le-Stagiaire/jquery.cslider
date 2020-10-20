1eda1c546544ff977ecfe6502cb1b4bffb0ee2e4
MichaIng <micha@dietpi.com>
2020-10-20 17:19:46 +0200

    Do not declare variables which are never used
    Signed-off-by: MichaIng <micha@dietpi.com>

--- src/jquery.cslider.js
+++ src/jquery.cslider.js
@@ -53,7 +53,7 @@
             }
         },
         _navigate: function (page, dir) {
-            var $current = this.$slides.eq(this.current), $next, _self = this;
+            var $current = this.$slides.eq(this.current), $next;
             if (this.current === page || this.isAnimating) return false;
             this.isAnimating = true;
             // check dir
@@ -112,11 +112,11 @@
         },
         _loadEvents: function () {
             var _self = this;
-            this.$pages.on('click.cslider', function (event) {
+            this.$pages.on('click.cslider', function () {
                 _self.page($(this).index());
                 return false;
             });
-            this.$navNext.on('click.cslider', function (event) {
+            this.$navNext.on('click.cslider', function () {
                 if (_self.options.autoplay) {
                     clearTimeout(_self.slideshow);
                     _self.options.autoplay = false;
@@ -125,7 +125,7 @@
                 _self._navigate(page, 'next');
                 return false;
             });
-            this.$navPrev.on('click.cslider', function (event) {
+            this.$navPrev.on('click.cslider', function () {
                 if (_self.options.autoplay) {
                     clearTimeout(_self.slideshow);
                     _self.options.autoplay = false;
